046be29565ed5d9745db3b86904c8a4cc14b449d
Tarek Alsibai <tarekalsibai1@gmail.com>
2021-05-13 04:25:05 +0300

    Update app.js

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ function a() {
       "You can make a donation on my bitcoin wallet : bc1qjrprrxea8tzysj6m6jfvyyq5yu3mvgj3vuwfu8"
     );
   };
+  
 }
 // function donate() {
 //   setTimeout(a, 7000);
@@ -43,6 +44,7 @@ function myPlay() {
 let id;
 let width = 0;
 function move() {
+  document.getElementById("namw").style.display = 'none';
   var elem = document.getElementById("myBar");
   width = 0;
   id = setInterval(frame, 1000);
@@ -58,6 +60,7 @@ function move() {
 }
 
 function startTimer(duration, display) {
+  
   let timer = duration,
     minutes,
     seconds;
@@ -81,10 +84,12 @@ function startTimer(duration, display) {
       clearInterval(id);
       width = 0;
       function start() {
+        
         if (!isClicked) {
           startTimer(pomodoro, display);
           isClicked = true;
           myPlay();
+          
           move();
         }
       }
@@ -123,6 +128,7 @@ function startTimer(duration, display) {
           startTimer(pomodoro, display);
           isClicked = true;
           myPlay();
+          
           clearInterval(id);
           width = 0;
           move();
@@ -146,3 +152,64 @@ function start() {
 }
 
 startButton.onclick = () => start();
+
+var canvas = {
+  element: document.getElementById("canvas"),
+  width: screen.width,
+  height: screen.height,
+  initialize: function () {
+    this.element.style.width = this.width;
+    this.element.style.height = this.height;
+    document.body.appendChild(this.element);
+  },
+};
+
+var Ball = {
+  create: function (color, dx, dy) {
+    var newBall = Object.create(this);
+    newBall.dx = dx;
+    newBall.dy = dy;
+    newBall.element = document.createElement("h1");
+    newBall.element.innerHTML = "MADS";
+    newBall.width = newBall.element.innerHTML.length*30;
+    newBall.height = screen.height/6;
+    newBall.element.style.fontFamily = "Teko, sans-serif";
+    newBall.element.style.color = color;
+    newBall.element.style.width = newBall.width + "px";
+    newBall.element.style.height = newBall.height + "px";
+    newBall.element.className += " ball";
+    newBall.width = parseInt(newBall.element.style.width);
+    newBall.height = parseInt(newBall.element.style.height);
+    canvas.element.appendChild(newBall.element);
+    return newBall;
+  },
+  moveTo: function (x, y) {
+    this.element.style.left = x + "px";
+    this.element.style.top = y + "px";
+  },
+  changeDirectionIfNecessary: function (x, y) {
+    if (x < 0 || x > canvas.width - this.width) {
+      this.dx = -this.dx;
+    }
+    if (y < 0 || y > canvas.height - this.height) {
+      this.dy = -this.dy;
+    }
+  },
+  draw: function (x, y) {
+    this.moveTo(x, y);
+    var ball = this;
+    setTimeout(function () {
+      ball.changeDirectionIfNecessary(x, y);
+      ball.draw(x + ball.dx, y + ball.dy);
+    }, 1000 / 60);
+  },
+};
+canvas.initialize();
+var ball1 = Ball.create("red", 4, 3);
+// var ball2 = Ball.create("red", 1, 5);
+// var ball3 = Ball.create("green", 2, 2);
+// document.getElementById("ooop").innerHTML = "my text"
+ball1.draw(0, 0);
+
+// ball2.draw(20, 200);
+// ball3.draw(300, 330);
